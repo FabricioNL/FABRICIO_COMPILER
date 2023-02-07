211d2234eeabf7cfa875e050a3d80249cc324a6d
Fabricio Neri <fabricionl@al.insper.edu.br>
2023-02-07 16:03:05 -0300

    adicionando atividade 1

---
+++ main.py
@@ -0,0 +1,55 @@
+import sys 
+string = sys.argv[1]
+
+def compiler(expression):  
+
+    #PRIMEIRA VERIFICACAO
+    if expression[0].isdigit() == False:
+        sys.exit(1)
+    
+    if expression[-1].isdigit() == False:
+        sys.exit(1)
+
+    #FLAGS
+    operator_in_row = False
+
+    split_expression = expression.split()
+    new_expression = ''
+
+    #SEGUNDA VERIFICACAO: DOIS NUMEROS SEGUIDOS
+    for i in range(len(split_expression)-1):
+            if (split_expression[i].isdigit() == True) and (split_expression[i+1].isdigit() == True):
+                print('NUMBER IN ROW')
+                sys.exit(1)
+
+    for i in split_expression:
+        new_expression += i
+
+    val_numeric = ''
+    expression_list = []
+
+    for i in new_expression:
+        if i.isdigit():
+            val_numeric += i
+        else:
+            expression_list.append(val_numeric)
+            expression_list.append(i)
+            val_numeric = ''
+
+    expression_list.append(val_numeric)
+
+    #TERCEIRA VERIFICACAO: DOIS OPERADORES SEGUIDOS
+    for i in range(len(expression_list)-1):
+        if (expression_list[i].isdigit() == False) and (expression_list[i+1].isdigit() == False):
+            print('OPERATOR IN ROW')
+            sys.exit(1)   
+
+    res = eval(new_expression)
+    #print(split_expression)
+    #print(new_expression)
+    #print(expression_list)
+    print(res)
+    
+
+compiler(string)
+    
